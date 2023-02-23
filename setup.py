acbc7b89c96e7c1d602d548abec1bd563270ce25
Qolorerr <56872573+Qolorerr@users.noreply.github.com>
2023-02-24 01:50:01 +0300

    Fix setup.py
    Adding LICENSE

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='filesocket',
     packages=['filesocket'] + ['filesocket.' + pkg for pkg in find_packages('filesocket')],
     version='0.1.0',
     description='Transferring data between PCs library',
-    author='Qolorer',
-    license='MIT',
+    long_description=long_description,
+    author='Mikhail Ivanov',
+    author_email='qolorer@gmail.com',
+    url='https://github.com/Qolorerr/FileSocketClient',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'requests == "2.28.1"',
         'python_version == "3.9"',
